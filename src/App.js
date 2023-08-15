d9953978fe06afcf10dee79bd65a6ece74b20277
abilov042 <shamilabilov599@gmail.com>
2023-08-15 23:21:42 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080/api/candidates/getAll").then(a => setData(a.data.data[0].email))
+  },[data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <h1>{data}</h1>
+   
+     
     </div>
   );
 }
